51108d0d02984e46c9f81139ef893256d3770c3d
Dev Bali <devbali02@gmail.com>
2022-01-18 23:03:21 -0800

    Flask Group Project without Testing

---
+++ Flask Group/src/app.py
@@ -0,0 +1,33 @@
+import json
+from flask import Flask
+
+####################
+### This block initializes the arrays "Users" and "Pleets" from the json file storage system
+### To edit any data, just edit these arrays, and call the update() method
+### If you ever entirely screw up your data file, just copy data.json from testing into src
+####################
+
+with open("data.json","r") as datafile:
+    rawdata = json.load(datafile)
+    Users = rawdata["users"]
+    Pleets = rawdata["pleets"]
+
+def update():
+    with open("data.json","w") as datafile:
+        json.dump({
+            "users": Users,
+            "pleets": Pleets
+        })
+
+####################
+### Flask work starts here
+### A Hello World method has been provided as starter, and can be replaced
+####################
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def hello_world ():
+    return "Hello World"
+
+app.run(port=5001)
