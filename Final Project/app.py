e281599b0a1d979e9966d9c8191da97b246639e7
Taiga Kitao <taiga2002@berkeley.edu>
2022-03-14 18:00:22 -0700

    taiga

--- Final Project/app.py
+++ Final Project/app.py
@@ -8,3 +8,23 @@ import time
 
 app = Flask(__name__)
 app.run(port=5001, debug=True)
+
+#TAIGA'S work
+@app.route("/pleets/<pleet_id>", methods=["DELETE"])
+def delete_plete(user_id):
+    for user in Users:
+        if user["_id"] == user_id:
+            Users.remove(user)
+            update()
+            return {"message": "User succesfully deleted"}, 200
+    return {"message": "Pleet not found!"}, 404
+    
+@app.route("/users/<user_id>", methods=["PUT"])
+def edit_put(user_id):
+    data = request.json
+    for user in Users:
+        if user["_id"] == user_id:
+            user["name"] = data["name"]
+            update()
+            return {"message": "User Profile Successfully edited!"}, 200
+    return {"message": "User not found!"}, 404
