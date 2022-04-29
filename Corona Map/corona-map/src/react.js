0fb319f9bd79f4b8fd382e63ed4a6d1fda71c2b3
Ichchitaa Sawrikar <ichchitaa@gmail.com>
2022-04-28 20:40:34 -0700

    commit

--- Corona Map/corona-map/src/react.js
+++ Corona Map/corona-map/src/react.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Chart from 'react-google-charts'
+import { Line } from "react-chartjs-2";
 
 //getData()
     // fetch("https://disease.sh//")
@@ -13,44 +14,57 @@ class MultiLineChart extends Component {
         super();
   
         this.state= {
-            data: 1
+            data: null
         };
     }
+
     componentDidMount() {
         // Simple GET request using fetch
         fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=all")
-            .then(response => 
+            .then(response => response.json())
+            .then(data2 => {
+                console.log(data2);
                 this.setState({
-                data: response
-            }))
+                    data: data2
+                });
+                
+            })
+            .catch(error => console.log(error))
     }
 
-    // var result = JSON.parse(response);
+    loadData() {
+        if (this.state.data) {
+            return (
+                <Line 
+                   data = {{
+                    labels : this.state.data.cases.keys,
+                    datasets: [
+                        {
+                            label: "Case numbers",
+                            data: this.state.data.cases.values,
+                            fill: false,
+                            borderWidth: 5,
+                            backgroundColor: 'yellow',
+                            borderColor: 'black',
+                            responsive: true
+                        },
+                    ],
+                }}
+                />  
+            )
+        }
+    }
 
-    // var dictionary = {}
-
-    // for (var key in result) {
-    //     dictionary[key] = result[key];
-    // }
-
-    // const lineData = []
-    // data.push(['Time', 'Cases'])
-    // for (var key in dictionary) {
-    //     data.push([key, dictionary[value]])
-    // }
-
-    // const lineOptions = {
-    //     xAxis: {
-    //         title: 'Time',
-    //     },
-    //     yAxis: {
-    //         title: 'Cases',
-    //     }
-    // }
+    lineGraph() {
+        return (
+            <div className="class">
+                <h1>This is plot</h1>
+                {this.loadData()}
+            </div>
+        );
+    }
 
     render() {
-        console.log("Hello world");
-        return 
             // <div className="class">
             //     <h2>Historical Data</h2>
             //     <Chart 
@@ -65,8 +79,13 @@ class MultiLineChart extends Component {
             // </div>
             // {this.state.data}
         //)
+        return this.lineGraph();
+
+        // export default LineGraph;
     }
 }
+
+
 export default MultiLineChart
 
 
