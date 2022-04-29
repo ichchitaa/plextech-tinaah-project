28dca95239d7050157fa2cca976bd1fd5b6ecb01
Ichchitaa Sawrikar <ichchitaa@gmail.com>
2022-04-28 21:13:51 -0700

    updated with images

--- Frontend Material UI/newapp/src/App.js
+++ Frontend Material UI/newapp/src/App.js
@@ -4,8 +4,14 @@ import React from 'react';
 import { Typography, CssBaseline, AppBar, Toolbar, Button, Link, Grid } from '@mui/material';
 import LocalDiningIcon from '@mui/icons-material/LocalDining';
 import { pink, purple, yellow } from '@mui/material/colors';
-import { Image } from 'mui-image';
+// import { Image } from 'mui-image';
 // import Link from '@mui/material/Link';
+// import Image from 'material-ui-image'
+
+import cafe3 from './cafe3.jpeg'
+import clarkkerr from './clarkkerr.jpeg'
+import foothill from './foothill.jpeg'
+import croads from './croads.jpeg'
 
 function App() {
   return (
@@ -18,7 +24,7 @@ function App() {
         <Toolbar>
           <LocalDiningIcon variant="h3" />
           <Typography variant="h5">
-            Welcome to Cal Eats!
+             Welcome to Cal Eats
           </Typography>
         </Toolbar>
       </AppBar>
@@ -44,18 +50,22 @@ function App() {
           <Grid item xs={6} style={{background:yellow}} id="grid2">
           <Typography id="name" variant="h3" align='center'>Cafe 3</Typography>
           <Typography variant="h5" align='center'>Average rating today:</Typography>
+          <img id="img1" src={cafe3} />
         </Grid>
         <Grid item xs={6} style={{background:pink}} id="grid1">
         <Typography variant="h3" align='center' id="name">Crossroads</Typography>
           <Typography variant="h5" align='center'>Average rating today:</Typography>
+          <img id="img2" src={croads} />
         </Grid>
         <Grid item xs={6} style={{background:purple}} id="grid1">
         <Typography variant="h3" align='center' id="name">Foothill</Typography>
           <Typography variant="h5" align='center'>Average rating today:</Typography>
+          <img id="img3" src={foothill} />
         </Grid>
         <Grid item xs={6} style={{background:purple}} id="grid2">
         <Typography variant="h3" align='center' id="name">Clark Kerr</Typography>
           <Typography variant="h5" align='center'>Average rating today:</Typography>
+          <img id="img4" src={clarkkerr} />
         </Grid>
           </Grid>
           {/* <Typography id="name" variant="h3" align='center'>Cafe 3</Typography>
