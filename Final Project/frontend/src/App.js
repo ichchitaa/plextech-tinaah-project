efed9a87ea28c8a343f7ba188947a5736b52a5b9
BingXu Meng <bingxumeng@BingXudeMacBook-Pro.local>
2022-05-01 15:00:37 -0700

    merged amal's work

--- Final Project/frontend/src/App.js
+++ Final Project/frontend/src/App.js
@@ -2,24 +2,49 @@ import FrontPage from "./components/FrontPage.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReviewPageTop from "./components/ReviewPageTop.js";
 import FormPage from "./components/FormPage.js";
+import Comments from "./components/Comments.js";
+import { Cookie } from "@mui/icons-material";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<FrontPage />} />
-        <Route path="cafe_3" element={<ReviewPageTop cafe_type="cafe_3" />} />
+        <Route
+          path="cafe_3"
+          element={
+            <div>
+              <ReviewPageTop cafe_type="cafe_3" />
+              <Comments />
+            </div>
+          }
+        />
         <Route
           path="crossroads"
-          element={<ReviewPageTop cafe_type="crossroads" />}
+          element={
+            <div>
+              <ReviewPageTop cafe_type="crossroads" />
+              <Comments />
+            </div>
+          }
         />
         <Route
           path="foothill"
-          element={<ReviewPageTop cafe_type="foothill" />}
+          element={
+            <div>
+              <ReviewPageTop cafe_type="foothill" />
+              <Comments />
+            </div>
+          }
         />
         <Route
           path="clark_kerr_campus"
-          element={<ReviewPageTop cafe_type="clark_kerr_campus" />}
+          element={
+            <div>
+              <ReviewPageTop cafe_type="clark_kerr_campus" />
+              <Comments />
+            </div>
+          }
         />
         <Route path="review_form" element={<FormPage />} />
       </Routes>
